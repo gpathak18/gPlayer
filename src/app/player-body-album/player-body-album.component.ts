3286abe881ef1cd06bab8887a6693af7ad1c140e
gpathak18 <gpathak@buffalo.edu>
2017-11-08 17:37:18 -0800

    v4.0.3

--- src/app/player-body-album/player-body-album.component.ts
+++ src/app/player-body-album/player-body-album.component.ts
@@ -6,11 +6,35 @@ import { PlayerService } from '../services/player.service';
 import { Track } from '../track';
 import { Observable } from 'rxjs';
 import Utility from '../Utility';
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+ 
 @Component({
   selector: 'app-player-body-album',
   templateUrl: './player-body-album.component.html',
-  styleUrls: ['./player-body-album.component.css']
+  styleUrls: ['./player-body-album.component.css'],
+  animations: [
+    trigger('flipStateTrigger', [
+      state('false', style({
+        zindex: '10px',
+        top: '10px',
+        left: '-50px',
+        height: '400px',
+        width: '400px',
+        transform: 'rotateY(180deg) translate(0px,0px)'
+      })),
+      state('true', style({
+        transform: 'rotateY(0)'
+      })),
+      transition('true => false', animate('1s ease-out')),
+      transition('false => true', animate('1s ease-in'))
+    ])
+  ]
 })
 export class PlayerBodyAlbumComponent implements OnInit {
 
@@ -20,7 +44,7 @@ export class PlayerBodyAlbumComponent implements OnInit {
   private tracks: Array<Track>;
   private track;
   private albums: any;
-  private isFront = true;
+  private isFlipped = true;
 
   constructor(  
     private playlistService: PlaylistService,
@@ -33,12 +57,14 @@ export class PlayerBodyAlbumComponent implements OnInit {
   ngOnInit() {
 
     this.datastore.currentTracks.subscribe(addedTracks => {
+
       this.tracks = addedTracks;
       this.track = this.tracks[0]
       const source = Observable.from(this.tracks);
       console.log('here it is',this.tracks)
       
       this.albums = source
+
       .groupBy((track:any) => {
         track.Album;
         console.log('here it is',track)   
@@ -50,8 +76,12 @@ export class PlayerBodyAlbumComponent implements OnInit {
     this.albums.subscribe(val => console.log(val));
   }
 
+
   private truncateString(str, len) {
     return Utility.truncateString(str, len);
   }
 
+  private flippingDone(e){
+    console.log(e)
+  }
 }
